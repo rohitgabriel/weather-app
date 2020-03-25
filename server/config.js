d84549fc3352bd16a56a81826cd9207d0d7eb9f3
unknown <suzieboo@gmail.com>
2020-03-26 01:25:20 +1300

    initial

---
+++ server/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  baseUrl: {
+    protocol: 'http',
+    hostname: 'api.openweathermap.org',
+    path: '/data/2.5/weather',
+  },
+
+  query: {
+    name: 'q',
+    id: 'id',
+    coordinates: {
+      latitude: 'lat',
+      longitude: 'lon',
+    },
+    zipcode: 'zip',
+  },
+
+  APIkey: '88d085e6ad5cbbb392e70ebe2d60cd61',
+};
