4332f4bf686fd16919227a8a06e31a910dbbc2c4
unknown <suzieboo@gmail.com>
2020-03-28 01:24:29 +1300

    var fix

--- get-instance-id.sh
+++ get-instance-id.sh
@@ -2,6 +2,6 @@
 iname=$(aws autoscaling describe-auto-scaling-groups --region ap-southeast-2 | jq '.AutoScalingGroups[0].Instances[0].InstanceId')
 instance_id=`echo $iname | cut -d '"' -f2`
 # echo $instance_id
-instance_ip=$(aws ec2 describe-instances --instance-ids i-06f04d63e6d0f1faa --region ap-southeast-2 | jq '.Reservations[0].Instances[0].PublicIpAddress' | cut -d '"' -f2)
+instance_ip=$(aws ec2 describe-instances --instance-ids $instance_id --region ap-southeast-2 | jq '.Reservations[0].Instances[0].PublicIpAddress' | cut -d '"' -f2)
 echo $instance_ip
 
