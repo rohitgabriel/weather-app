0e8d2127aecae0a4c12f898a536fdbb2009cd5c6
unknown <suzieboo@gmail.com>
2020-03-29 00:07:51 +1300

    azure deployment

---
+++ Jenkinsfileazure.groovy
@@ -0,0 +1,53 @@
+pipeline {
+    agent any
+    environment {
+        AZURE_CRED=credentials('AZURE_CRED')
+        AZURE_ID=credentials('AZURE_ID')
+        AZUREPUB=credentials('AZUREPUB')
+        client_id="${AZURE_CRED_USR}"
+        client_secret="${AZURE_CRED_PSW}"
+        tenant_id="${AZURE_ID_USR}"
+        subscription_id="${AZURE_ID_PSW}"
+        location="australiaeast"
+    }
+    stages {
+        // stage('Checkout repo') {
+        //     steps {
+        //         git branch: 'master',
+        //         credentialsId: 'mygitcredid',
+        //         url: 'https://github.com/rohitgabriel/aws-deployment.git'
+        //     }
+        // }
+        // stage ('Deploy') {
+        //     steps{
+        //         sshagent(credentials : ['awskey']) {
+        //         sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.54.226.2 uptime'
+        //         sh 'ssh -v ubuntu@13.54.226.2'
+        //         sh 'scp ./get-instance-id.sh ubuntu@13.54.226.2:/tmp/target'
+        //         }
+        //     }
+        // }
+        stage("Get Instance IP and setup script") {
+            steps {
+                {
+                script {
+                    instanceIP = sh(script: './getazure-instance-id.sh', returnStdout: true).trim()
+                }
+                echo "${instanceIP}"
+                }
+                sshagent(credentials : ['azurekey']) {
+                    sh "ssh -o StrictHostKeyChecking=no ubuntu@${instanceIP} uptime"
+                    sh "scp ./deploycode.sh ubuntu@${instanceIP}:/tmp/deploycode.sh"
+                    sh "ssh ubuntu@${instanceIP} chmod 755 /tmp/deploycode.sh"
+                }
+            }
+        }
+        stage("Deploy WeatherApp") {
+            steps {
+                sshagent(credentials : ['azurekey']) {
+                    sh "ssh ubuntu@${instanceIP} /tmp/deploycode.sh"
+                }
+            }
+        }
+    }
+}
---
+++ getazure-instance-id.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# iname=$(aws autoscaling describe-auto-scaling-groups --region ap-southeast-2 | jq '.AutoScalingGroups[0].Instances[0].InstanceId')
+# instance_id=`echo $iname | cut -d '"' -f2`
+
+# instance_ip=$(aws ec2 describe-instances --instance-ids $instance_id --region ap-southeast-2 | jq '.Reservations[0].Instances[0].PublicIpAddress' | cut -d '"' -f2)
+
+# For Azure, using a static IP
+export instance_ip="104.210.74.172"
+echo $instance_ip
+
